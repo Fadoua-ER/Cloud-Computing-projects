e153095338559afc284bf0b58dc49dea50d939b9
Fadoua ER-RAMY <114744738+Fadoua-ER@users.noreply.github.com>
2025-05-25 12:40:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
 def home():
     return "Bonjour depuis Render : Votre app Flask est bien en ligne"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
